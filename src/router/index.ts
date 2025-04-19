01e100e181f0fd389b554c895892d64b7507ceec
PKTseng <jk0910948659@gmail.com>
2025-04-20 01:46:12 +0800

    設定 router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,44 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'base',
+    component: () => import('@/layout/index.vue'),
+    children: [
+      {
+        path: '',
+        name: 'default',
+        redirect: '/nft',
+      },
+      {
+        path: '/nft',
+        name: 'nft',
+        component: () => import('@/views/NFTList.vue'), // NFT 列表
+      },
+      {
+        path: '/nft/:id',
+        name: 'nft-detail',
+        component: () => import('@/views/NftDetail.vue'), // NFT 詳情頁
+        props: true,
+      },
+      {
+        path: '/mint',
+        name: 'mint',
+        component: () => import('@/views/Mint.vue'), // NFT 鑄造頁
+      },
+      {
+        path: '/manage',
+        name: 'manage',
+        component: () => import('@/views/Manage.vue'), // 上架/下架管理
+      },
+    ],
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+  routes,
 })
 
 export default router
