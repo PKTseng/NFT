1a0b08e3da8a9eb7b34863f9c01e325d32f66cb5
PKTseng <jk0910948659@gmail.com>
2025-04-20 12:09:37 +0800

    update style

--- src/views/NFTList.vue
+++ src/views/NFTList.vue
@@ -1,11 +1,43 @@
 <template>
-  <div v-if="loading">載入中...</div>
-  <div v-else-if="assets.length > 0" class="grid grid-cols-3 gap-4">
-    <div v-for="asset in assets" :key="asset.id" class="border p-4 rounded">
-      <img v-if="getImageUrl(asset)" :src="getImageUrl(asset) ?? ''" alt="NFT" class="w-full h-auto" />
-      <h3 class="mt-2 font-bold">{{ asset.id.slice(0, 8) }}...</h3>
+  <div
+    v-if="assets.length > 0"
+    class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 min-h-screen"
+  >
+    <div
+      v-for="asset in assets"
+      :key="asset.id"
+      class="rounded-xl cursor-pointer overflow-hidden flex flex-col bg-white"
+    >
+      <!-- 圖片 -->
+      <div class="overflow-hidden rounded-t-xl h-full">
+        <img
+          :src="getImageUrl(asset) ?? ''"
+          alt="asset image"
+          class="transition-transform duration-300 hover:scale-125 object-cover w-full h-full"
+        />
+      </div>
+
+      <!-- 資訊 -->
+      <div class="flex flex-col justify-between bg-gray-100 pa-4 h-[200px]">
+        <!-- 名稱 -->
+        <p class="font-bold text-xl">{{ asset.content.metadata.name }}</p>
+
+        <!-- Collection 名稱 -->
+        <p class="text-gray-600">Collection: {{ asset.grouping?.[0]?.collection_metadata?.name || 'Uncategorized' }}</p>
+
+        <!-- 擁有者地址 -->
+        <p class="text-gray-600">
+          Owner: {{ asset.ownership.owner.slice(0, 4) }}...{{ asset.ownership.owner.slice(-4) }}
+        </p>
+
+        <!-- 分潤比例 -->
+        <p v-if="asset.royalty?.percent" class="text-gray-600">
+          Royalty: {{ (asset.royalty.percent * 100).toFixed(1) }}%
+        </p>
+      </div>
     </div>
   </div>
+
   <div v-else>沒有找到NFT資產</div>
 </template>
 
@@ -13,9 +45,9 @@
 import { useGetAssetsByOwner } from '@/hooks/UseGetAssetsByOwner'
 import { onMounted } from 'vue'
 
-const { assets, loading, fetchAssetsByOwner, getImageUrl } = useGetAssetsByOwner()
+const { assets, fetchAssetsByOwner, getImageUrl } = useGetAssetsByOwner()
 
 onMounted(async () => {
-  await fetchAssetsByOwner(1, 10)
+  await fetchAssetsByOwner(1, 50)
 })
 </script>
