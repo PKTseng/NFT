10aec574f18aac3f3d9c7143ede380158292470e
PKTseng <jk0910948659@gmail.com>
2025-04-21 13:54:20 +0800

    錢包地址改成動態

--- src/api/GetAssetsByOwner.ts
+++ src/api/GetAssetsByOwner.ts
@@ -2,13 +2,13 @@ import { axiosIns } from '@/utils/request'
 
 const API_KEY = import.meta.env.VITE_APP_API_KEY
 
-export const getAssetsByOwner = (page: number = 1, limit: number = 50) => {
+export const getAssetsByOwner = (ownerAddress: string, page: number = 1, limit: number = 50) => {
   const payload = {
     jsonrpc: '2.0', // JSON-RPC 協議版本，用於標準化遠程過程調用
     id: 'text', // 請求的唯一識別符，用於對應請求和響應
     method: 'getAssetsByOwner', // API 方法名稱（此處未指定具體值）
     params: {
-      ownerAddress: '86xCnPeV69n6t3DnyGvkKobf9FdN2H9oiVDdaMpo2MMY', // 目標錢包地址（Solana 格式）
+      ownerAddress, // 目標錢包地址（Solana 格式）
 
       page, // 分頁參數：當前頁碼（此處未指定具體值）
       limit, // 分頁參數：每頁顯示的項目數量（此處未指定具體值）
