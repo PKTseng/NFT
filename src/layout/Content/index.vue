b0fda46ca0cfe1ec27fa8dbaad39e45dcc623178
PKTseng <jk0910948659@gmail.com>
2025-04-21 13:54:35 +0800

    移除背景顏色

--- src/layout/Content/index.vue
+++ src/layout/Content/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <main class="pa-6 bg-black h-full">
+  <main class="pa-6 h-full">
     <router-view />
   </main>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
