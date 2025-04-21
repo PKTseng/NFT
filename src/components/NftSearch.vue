32331f4669085bb53cedb1509e19751a17b8b05f
PKTseng <jk0910948659@gmail.com>
2025-04-21 11:23:19 +0800

    新增輸入框搜尋組件

---
+++ src/components/NftSearch.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="max-w-xl mx-auto">
+    <h2 class="text-xl font-bold">🔍 查詢 NFT（依照錢包地址）</h2>
+
+    <div class="flex items-center gap-2">
+      <v-text-field
+        v-model="ownerAddress"
+        type="text"
+        density="compact"
+        placeholder="輸入錢包地址（ownerAddress）"
+        class="w-full px-4 py-2 border rounded"
+        hide-details
+        variant="outlined"
+      />
+      <v-btn class="bg-blue-600 px-4 py-2 rounded hover:bg-blue-700" @click="searchText">查詢</v-btn>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const ownerAddress = ref('')
+
+const emit = defineEmits(['handleSearch'])
+
+const searchText = () => {
+  emit('handleSearch', ownerAddress.value)
+}
+</script>
