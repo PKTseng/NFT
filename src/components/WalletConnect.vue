77fe07e55373d187eb98b0fd9d575cefd5d497cc
PKTseng <jk0910948659@gmail.com>
2025-04-21 10:04:55 +0800

    update vue attr

--- src/components/WalletConnect.vue
+++ src/components/WalletConnect.vue
@@ -2,8 +2,8 @@
   {{ address }}
   <button
     v-if="!connected"
+    class="bg-gradient-to-r from-pink-500 to-yellow-500 text-white text-xl font-bold px-4 py-2 rounded-2xl shadow-lg hover:scale-110 hover:rotate-1 transition-all duration-300"
     @click="connectWallet"
-    class="bg-gradient-to-r from-pink-500 to-yellow-500 text-white text-xl font-bold px-8 py-4 rounded-2xl shadow-lg hover:scale-110 hover:rotate-1 transition-all duration-300"
   >
     Connect Wallet
   </button>
