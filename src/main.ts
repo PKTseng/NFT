04d2d7327afe2ff33fa31a4fbb7c5dacef389667
PKTseng <jk0910948659@gmail.com>
2025-04-21 12:06:31 +0800

    add mdi icon

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import './style/main.css'
+import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
 
 import App from './App.vue'
 import router from './router'
@@ -14,6 +16,13 @@ import router from './router'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 const app = createApp(App)
