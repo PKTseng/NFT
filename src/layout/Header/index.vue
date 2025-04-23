e105120a5d281faaa36ddeda9730dab1733f8a10
PKTseng <jk0910948659@gmail.com>
2025-04-23 16:24:52 +0800

    hidden 系列分類

--- src/layout/Header/index.vue
+++ src/layout/Header/index.vue
@@ -62,7 +62,7 @@ const isMenuOpen = ref(false)
 
 const navLinks = [
   { path: '/creator', label: '創作者作品' },
-  { path: '/series', label: '系列分類' },
+  // { path: '/series', label: '系列分類' },
   { path: '/mint', label: 'Mint NFT' },
   // { path: '/manage', label: '販售管理' },
 ]
