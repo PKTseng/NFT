d58d004cbc29b1f36a62e4286387b6999f67a4c4
PKTseng <jk0910948659@gmail.com>
2025-04-23 17:32:09 +0800

    update global type

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,27 +1,17 @@
-import { PublicKey } from '@solana/web3.js'
-
-declare global {
-  interface Window {
-    solana?: {
-      isPhantom?: boolean
-      isConnected: boolean
-      publickey: PublicKey
-      connect: () => Promise<{ publicKey: PublicKey }>
-      disconnect: () => Promise<void>
-      handleNotification: (e: any) => Promise<void>
-      removeAllListeners: (e: string) => void
-      request: (request: { method: string; params?: any }) => Promise<any>
-      signAllTransactions: (e: any[]) => Promise<any[]>
-      signAndSendAllTransactions: (e: any, t: any) => Promise<any>
-      signAndSendTransaction: (e: any, t: any) => Promise<any>
-      signIn: (e?: any) => Promise<any>
-      signMessage: (e: any, t?: string) => Promise<any>
-      signTransaction: (e: any) => Promise<any>
-      on(event: 'connect', listener: (publicKey: PublicKey) => void): void
-      on(event: 'disconnect', listener: () => void): void
-      on(event: 'accountChanged', listener: (publicKey: PublicKey) => void): void
-    }
-  }
+interface PhantomProvider {
+  isPhantom?: boolean
+  publicKey?: { toString(): string }
+  isConnected?: boolean
+  signTransaction: (transaction: any) => Promise<any>
+  signAllTransactions: (transactions: any[]) => Promise<any[]>
+  signMessage: (message: Uint8Array) => Promise<{ signature: Uint8Array }>
+  connect: () => Promise<{ publicKey: { toString(): string } }>
+  disconnect: () => Promise<void>
+  on: (event: string, callback: (...args: any[]) => void) => void
 }
 
-export {}
+interface Window {
+  phantom?: {
+    solana?: PhantomProvider
+  }
+}
